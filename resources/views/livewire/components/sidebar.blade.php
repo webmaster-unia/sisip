9181b9a08a7cf8fa9e67199a4f463e63da76430b
PABLITO <ramosruizpablo3@gmail.com>
2024-03-22 09:36:06 -0500

    cambio de IP
    Primer cambio IP

--- resources/views/livewire/components/sidebar.blade.php
+++ resources/views/livewire/components/sidebar.blade.php
@@ -45,17 +45,17 @@
                 </div>
                 <div class="mt-2 mb-4 mb-lg-0 w-full ps-3">
                     <a href="?theme=dark" class="btn w-100 hide-theme-dark mb-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
-                            stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                             <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
                         </svg>
                     </a>
                     <a href="?theme=light" class="btn w-100 hide-theme-light mb-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
-                            stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                             <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
                             <path
@@ -137,6 +137,30 @@
                             </div>
                         </div>
                     </div>
+                </li>
+                <li class="nav-item {{ request()->routeIs('ip.index') ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ route('ip.index') }}">
+                        <span class="nav-link-icon d-md-none d-lg-inline-block">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round"
+                                class="icon icon-tabler icons-tabler-outline icon-tabler-wiper-wash">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M12 20m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+                                <path d="M3 11l5.5 5.5a5 5 0 0 1 7 0l5.5 -5.5a12 12 0 0 0 -18 0" />
+                                <path d="M12 20l0 -14" />
+                                <path d="M4 6a4 4 0 0 1 .4 -1.8" />
+                                <path d="M7 2.1a4 4 0 0 1 2 0" />
+                                <path d="M12 6a4 4 0 0 0 -.4 -1.8" />
+                                <path d="M12 6a4 4 0 0 1 .4 -1.8" />
+                                <path d="M15 2.1a4 4 0 0 1 2 0" />
+                                <path d="M20 6a4 4 0 0 0 -.4 -1.8" />
+                            </svg>
+                        </span>
+                        <span class="nav-link-title">
+                            IP
+                        </span>
+                    </a>
                 </li>
                 <hr class="ms-lg-3 my-3">
             </ul>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Livewire\Home\Index as HomeIndex;
 use App\Livewire\Configuracion\Usuario\Index as ConfiguracionUsuarioIndex;
 use App\Livewire\Configuracion\Rol\Index as ConfiguracionRolIndex;
 use App\Livewire\Configuracion\Permiso\Index as ConfiguracionPermisoIndex;
+use App\Livewire\Ip\Index as IpIndex;
 
 // ruta que redirige al home
 Route::redirect('/', '/home');
@@ -32,4 +33,9 @@ Route::prefix('configuracion')->group(function () {
     Route::get('/permisos', ConfiguracionPermisoIndex::class)
         ->middleware('auth')
         ->name('configuracion.permiso.index');
+
+    //ruta para IP
+    Route::get('/ip',IpIndex::class)
+    ->middleware('auth')
+    ->name('ip.index');
 });
