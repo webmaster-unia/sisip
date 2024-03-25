1bc7472f1afe3edc427c0a00d251ec51ae8a66c5
PABLITO <ramosruizpablo3@gmail.com>
2024-03-25 09:48:13 -0500

    cambio IP
    Cambio boton modal IP

--- app/Livewire/Ip/Index.php
+++ app/Livewire/Ip/Index.php
@@ -7,8 +7,33 @@ use Livewire\Component;
 class Index extends Component
 {
 
-    public $button_modal='crear ip';
 
+    public $button_modal = 'crear ip';
+
+    public $modo = 'create';
+
+    public $ip;
+    public $is_active;
+
+    public function create()
+    {
+        $this->limpiar_modal();
+        $this->modo = 'create';
+        $this->button_modal = 'crear ip';
+        $this->resetErrorBag();
+        $this->resetValidation();
+    }
+
+    public function limpiar_modal()
+    {
+        $this->reset([
+            'ip',
+            'is_active'
+        ]);
+
+        $this->resetErrorBag();
+        $this->resetValidation();
+    }
     public function render()
     {
         return view('livewire.ip.index');
