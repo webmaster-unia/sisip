f4071a96540db32c126e07fc1fe3f6cd6970519e
LuisMasluCan <139094450+LuisMasluCan@users.noreply.github.com>
2024-03-25 09:06:48 -0500

    cambiosi
    cambio

--- app/Livewire/Area/Index.php
+++ app/Livewire/Area/Index.php
@@ -30,6 +30,28 @@ class Index extends Component
     public $button_modal = 'Crear area';
     public $modo = 'create';
 
+
+    //variables para formulario
+    public $name;
+    public $slug;
+    public $abreviation;
+    public $cantidad;
+    public $ip_inicio;
+    public $ip_fin;
+    public $id_active;
+
+
+    //crear rol
+    public function create(){
+        $this->limpiar_modal();
+        $this->modo = 'create';
+        $this->title_modal = 'Crear nueva Area';
+        $this->button_modal = 'Crear area';
+        $this->resetErrorBag();
+        $this->resetValidation();
+    }
+
+
     public function render()
     {
         return view('livewire.area.index');
