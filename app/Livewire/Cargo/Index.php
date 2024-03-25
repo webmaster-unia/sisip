dbf02a17633a0c4d5fb8bd20053ea7a6adbfce79
chato09 <ruizjohan531@gmail.com>
2024-03-25 10:08:43 -0500

    wasa
    cambio4

--- app/Livewire/Cargo/Index.php
+++ app/Livewire/Cargo/Index.php
@@ -13,6 +13,65 @@ use Livewire\Component;
 class Index extends Component
 {
     public $button_Cargo='Crear Cargo';
+
+    public $modo = 'create';
+
+    public $name_cargo;
+
+    public $area_ip_id;
+
+    public $apellido_paterno;
+
+    public $apellido_materno;
+
+    public $nombre;
+
+    public $dni;
+
+    public $correo_institucional;
+
+    public $nombre_equipo;
+
+    public $usuario_red;
+
+    public $procesador;
+
+    public $memoria;
+
+    public $sistema_opreativo;
+
+    public $mac_dispositivo;
+
+    public $is_active;
+
+    public function create(){
+        $this->limpiar_moda();
+        $this->modo ='create';
+        $this->button_Cargo='crear cargo';
+        $this->resetErrorBag();
+        $this->resetValidation();
+    }
+    public function limpiar_moda(){
+        $this->reset([
+            'name_cargo',
+            'area_ip_id',
+            'apellido_paterno',
+            'apellido_materno',
+            'nombre',
+            'dni',
+            'correo_institucional',
+            'nombre_equipo',
+            'usuario_red',
+            'procesador',
+            'memoria',
+            'sistema_opreativo',
+            'mac_dispositivo',
+            'is_active'
+
+        ]);
+        $this->resetErrorBag();
+        $this->resetValidation();
+    }
     public function render()
     {
         return view('livewire.cargo.index');
