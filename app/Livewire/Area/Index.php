c53fd202c0271e61fbc092b078a2488f01a8d1cd
chato09 <ruizjohan531@gmail.com>
2024-04-02 11:49:22 -0500

    Cargo
    falta editar, eliminar y guardar

--- app/Livewire/Area/Index.php
+++ app/Livewire/Area/Index.php
@@ -128,7 +128,6 @@ class Index extends Component
         $area->ip_inicio = $this->ip_inicio;
         $area->ip_fin = $this->ip_fin;
         $area->save();
-
         $this->limpiar_modal();
     }
 
